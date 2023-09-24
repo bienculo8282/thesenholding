6f8481739e454971ac31b42ed49386b7e4ec06b4
bienculo8282 <139576246+bienculo8282@users.noreply.github.com>
2023-09-24 04:46:34 -0400

    dwa

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ module.exports = {
     return [
       {
         source: '/posts/:slug',
-        destination: 'https://dailyxpresss.com/:slug',
+        destination: 'https://thesenholding.com/:slug',
         has: [
           {
             type: 'header',
